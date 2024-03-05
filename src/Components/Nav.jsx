c05806d1c57bebb1374a276fafbaba3ff31d42e8
Aakash Ojha <oaakash14@gmail.com>
2024-03-05 16:56:38 +0530

    Added Images to File

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,25 @@
+
+
+const Navigation = () => {
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="" />
+        </div>
+        <ul>
+          <li href ="#">Home</li>
+          <li href ="#">Location</li>
+          <li href ="#">About</li>
+          <li href ="#">Contact</li>
+        </ul>
+       <div className="Btn">
+       <button className="btn-login">Login</button>
+       
+       </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navigation;
